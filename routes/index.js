de10cbb27795d8b3b231e57962812a06fbee0958
mhf <2639482456@qq.com>
2017-07-20 20:09:02 +0800

    数据库链接测试成功，跨域模块安装，-dev写入

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host:'10.0.44.252',
+    user:'kkmovie',
+    password:'123',
+    database:'kmovie'
+})
+conn.connect(function(err){
+    if (err) {
+        throw err;
+    }else{
+        console.log('连接成功');
+
+    }
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/d/w',function(req,res){
+    let movies = `SELECT * FROM movie limit 0,4`;
+    conn.query(movies,(err,result)=>{
+        if(err){
+            return res.send({message:'false'})
+        }else{
+            return res.send({data:result});
+}
+})
+})
 
 module.exports = router;

65995e8da7e03ba449f90e96cd92a1e14a1c5785
work <840181150@qq.com>
2017-07-20 21:26:48 +0800

    专用路由

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var conn=require('../mysql/db');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/c/a',function (req,res) {
+    let sql = `SELECT * FROM movielist`;
+    conn.query(sql,function(err,result){
+        if(err){
+            res.send({success:false,data:err.message});
+        }else{
+            // console.log(result);
+            res.send({success:true,data:result});
+        }
+    });
+})
+
+router.get('/c/m',function (req,res) {
+    let sql = `SELECT * FROM movie`;
+    conn.query(sql,function(err,result){
+        if(err){
+            res.send({success:false,data:err.message});
+        }else{
+            console.log(result);
+            res.send({success:true,data:result});
+        }
+    });
+})
 
 module.exports = router;
